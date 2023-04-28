dfa4027cee68193397b33529a3ff3a76136489b2
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-04-28 23:00:53 +0200

    Rename blue to purple

--- src/enum/colors.py
+++ src/enum/colors.py
@@ -4,6 +4,6 @@ from enum import Enum
 class COLORS(Enum):
     WHITE = (255, 255, 255)
     RED = (200, 0, 0)
-    BLUE1 = (0, 0, 255)
-    BLUE2 = (0, 100, 255)
+    BLUE = (0, 0, 255)
+    PURPLE = (0, 100, 255)
     BLACK = (0, 0, 0)
