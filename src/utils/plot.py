f701dcfe006b0d5de4b8a4ad9493c58b48606970
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-04-30 23:37:16 +0200

    Add high score to plot

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -23,14 +23,14 @@ def plot(scores, mean_scores, highscore):
     plt.xlabel('# games')
     plt.ylabel('Score')
 
-    plt.plot(scores, label="Score", c="mediumvioletred", linewidth=2)
-    plt.plot(mean_scores, label="Mean score", c="#6A3D9A", linewidth=2)
+    plt.plot(scores, label="Score", c="#6A3D9A", linewidth=2)
+    plt.plot(mean_scores, label="Mean score", c="mediumvioletred", linewidth=2)
     plt.ylim(ymin=0)
 
     plt.legend(frameon=False)
 
     plt.text(len(scores)-1, scores[-1], str(scores[-1]))
-    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
-    plt.text(0, highscore, f'High score: {highscore}')
+    plt.text(len(mean_scores)-1, mean_scores[-1], str(round(mean_scores[-1], ndigits=2)))
+    plt.text(0, highscore*0.85, f'High score: {highscore}')
     plt.show(block=False)
     plt.pause(.1)
