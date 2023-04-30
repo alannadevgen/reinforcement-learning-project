8e1c1dc6f960142363e1d272ace7c645736d800f
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-04-30 22:07:59 +0200

    Update plot options

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -4,7 +4,7 @@ from IPython import display
 plt.ion()
 
 
-def plot(scores, mean_scores):
+def plot(scores, mean_scores, highscore):
     '''
     Plot the game results.
 
@@ -19,17 +19,18 @@ def plot(scores, mean_scores):
     display.display(plt.gcf())
     plt.clf()
 
-    plt.title("Training")
+    plt.title("Training snake AI")
     plt.xlabel('# games')
     plt.ylabel('Score')
 
-    plt.plot(scores, label="Score")
-    plt.plot(mean_scores, label="Mean score")
+    plt.plot(scores, label="Score", c="mediumvioletred", linewidth=2)
+    plt.plot(mean_scores, label="Mean score", c="#6A3D9A", linewidth=2)
     plt.ylim(ymin=0)
-    
+
     plt.legend(frameon=False)
 
     plt.text(len(scores)-1, scores[-1], str(scores[-1]))
     plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    plt.text(0, highscore, f'High score: {highscore}')
     plt.show(block=False)
     plt.pause(.1)
